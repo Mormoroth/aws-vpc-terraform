8ba30efb21f1463f948a12d4002ce7f50b8de859
Morteza Khazamipour <mormoroth@gmail.com>
2021-11-23 08:01:49 -0800

    adding default cidr variables

--- variables.tf
+++ variables.tf
@@ -6,3 +6,27 @@ variable "region" {
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  default = "10.0.4.0/24"
+}
+
+variable "ec2_instance_type" {
+  default = "t2.micro"
+}
